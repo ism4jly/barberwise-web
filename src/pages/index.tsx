510aac30e8100b62f21805cf7bc77955ea10df66
ism4jly <ismaelpaulofreitas@gmail.com>
2025-11-04 15:48:02 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home(){
+    return(
+        <>
+        <Head>
+            <title>BarberWise - Seu sistema completo</title>
+        </Head>
+            <div>
+                <h1>Teste Projeto</h1>
+            </div>
+        </>
+    )
+}
