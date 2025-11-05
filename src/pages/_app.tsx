389ea5a995ed88afbbfba6b6aa98761384f6a1a0
ism4jly <ismaelpaulofreitas@gmail.com>
2025-11-05 10:44:08 -0300

    feat: configuração inicial do context api para autenticação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import { Provider } from "../components/ui/provider";
 
+import { AuthProvider } from "@/context/AuthContext";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </Provider>
   );
 }
