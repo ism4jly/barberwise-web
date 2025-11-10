57ba8b9573e1f5b4f106d169d969d864c8b6bd19
ism4jly <ismaelpaulofreitas@gmail.com>
2025-11-10 10:50:25 -0300

    feat: registrar cliente

---
+++ src/pages/new/index.tsx
@@ -0,0 +1,120 @@
+import { useMemo, useState } from "react";
+import Head from "next/head";
+import { Sidebar } from "@/components/sidebar";
+
+import {
+    Flex,
+    Text,
+    Heading,
+    Button,
+    Input,
+    Select,
+    Portal,
+    createListCollection
+} from '@chakra-ui/react'
+
+
+export default function New(){
+
+    const [customer, setCustomer] = useState('');
+
+    const collection = useMemo(() => {
+        return createListCollection({
+        items: [
+            { name: "Barba completa", value: "barba_completa" },
+            { name: "Corte degradê", value: "corte_degrade" },
+            { name: "Sobrancelha", value: "sobrancelha" },
+        ],
+        itemToString: (item) => item.name,
+        itemToValue: (item) => item.name,
+        })
+    }, [])
+
+    return(
+        <>
+            <Head>
+                <title>BarberWise - Novo agendamento</title>
+            </Head>
+            <Sidebar>
+                <Flex direction="column" align="flex-start" justify="flex-start">
+                    <Flex
+                        w="100%"
+                        direction="row"
+                        align="center"
+                        justify="flex-start"
+                    >
+                        <Heading fontSize="3xl" mt={4} mb={4} mr={4} color="white">
+                            Novo corte
+                        </Heading>
+                    </Flex>
+
+                    <Flex
+                        maxW="700px"
+                        pt={8}
+                        pb={8}
+                        w="100%"
+                        bg="barber.400"
+                        direction="column"
+                        align="center"
+                        justify="center"
+                    >
+                        <Input
+                            placeholder="Nome do cliente"
+                            w="85%"
+                            mb={3}
+                            size="lg"
+                            type="text"
+                            bg="barber.900"
+                            borderColor="gray.700"
+                            value={customer}
+                            onChange={(e) => setCustomer(e.target.value)}
+                        />
+                        <Select.Root
+                            collection={collection}
+                            size="lg"
+                            width="85%"
+                            mb={3}
+                        >
+                            <Select.HiddenSelect />
+                            <Select.Label color="white" fontWeight="bold">Selecione o corte</Select.Label>
+
+                            <Select.Control bg="barber.900">
+                                <Select.Trigger color="white" borderColor="gray.700">
+                                <Select.ValueText placeholder="Escolha um corte" />
+                                </Select.Trigger>
+                                <Select.IndicatorGroup>
+                                <Select.Indicator />
+                                </Select.IndicatorGroup>
+                            </Select.Control>
+
+                            <Portal>
+                                <Select.Positioner>
+                                <Select.Content>
+                                    {collection.items.map((item) => (
+                                    <Select.Item item={item} key={item.value}>
+                                        {item.name}
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                    ))}
+                                </Select.Content>
+                                </Select.Positioner>
+                            </Portal>
+                        </Select.Root>
+
+                        <Button
+                            w="85%"
+                            size="lg"
+                            color="gray.900"
+                            bg="button.cta"
+                            _hover={{ bg: "#ffb13e" }}
+                        >
+                            Cadastrar
+                        </Button>
+
+                    </Flex>
+
+                </Flex>
+            </Sidebar>
+        </>
+    )
+}
